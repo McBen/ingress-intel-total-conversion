098135444a3bb2b7a8d6e8d3fdc57acfd440373b
McBen <viertelvor12@gmx.net>
2026-09-17 13:06:14 +0200

    build: add postcss as css postprocessor

--- core/total-conversion-build.js
+++ core/total-conversion-build.js
@@ -220,7 +220,7 @@ document.head.innerHTML =
   '@include_css:external/leaflet.css@' +
   '</style>' +
   '<style>' +
-  '@include_string:style.css@' +
+  '@include_css:style.css@' +
   '</style>' +
   // note: smartphone.css injection moved into code/smartphone.js
   '<link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Roboto:100,100italic,300,300italic,400,400italic,500,500italic,700,700italic&subset=latin,cyrillic-ext,greek-ext,greek,vietnamese,latin-ext,cyrillic"/>';
